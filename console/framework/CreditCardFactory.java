077aac1109f60b020d4f4e14c3b7f3405bcf6f99
My Truong <ngocmytrg@gmail.com>
2024-05-23 13:30:15 -0500

    Remove unuse files

--- console/framework/CreditCardFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.mum.cs.cs525.labs.exercises.project.console.framework;
-
-import edu.mum.cs.cs525.labs.exercises.project.console.credit.CreditCard;
-
-public interface CreditCardFactory {
-    CreditCard createCreditCard(String accountNumber, double balance, double minimumPaymentPercentage, Customer customer);
-}
